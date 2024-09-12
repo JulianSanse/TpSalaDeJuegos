27913221231c43ec2fac55e3884941d234aaf7a9
Juliansanse <juliansanse4@gmail.com>
2024-09-12 14:54:00 -0300

    Roteo y cambio favicon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home.component';
+import { AboutmeComponent } from './components/aboutme-component/aboutme.component';
+import { LoginComponent } from './components/login-component/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [    
+    { path: '', redirectTo: '/home', pathMatch: "full" },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutmeComponent },
+    { path: 'login', component: LoginComponent},
+    
+    { path: '**', redirectTo: '/home', pathMatch:"full" }
+    ];
---
+++ src/app/components/aboutme-component/aboutme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutme-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './aboutme.component.html',
+  styleUrl: './aboutme.component.scss'
+})
+export class AboutmeComponent {
+
+}
