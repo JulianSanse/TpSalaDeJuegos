b2e36d8e2dbb7ee1b9d5fc45a85149e1425d11ef
Juliansanse <juliansanse4@gmail.com>
2024-09-29 14:15:09 -0300

    Agrego la base de datos de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet,RouterLink,HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/enviromentConfig.ts
@@ -0,0 +1,1 @@
+export const enviromentConfig = {"projectId":"tp-sala-de-juegos-f9e4d","appId":"1:952638774994:web:fc2d38e8178217237f970b","storageBucket":"tp-sala-de-juegos-f9e4d.appspot.com","apiKey":"AIzaSyBBqvZkSjd6w3XeaxzkyB97iBre2UAfjIw","authDomain":"tp-sala-de-juegos-f9e4d.firebaseapp.com","messagingSenderId":"952638774994"};
